ade559cd1f997139a8e701d1619cd7b43fd9f071
mkkim68 <116176465+mkkim68@users.noreply.github.com>
2024-01-30 17:50:28 +0900

    [Silver V] Title: 숫자 카드, Time: 608 ms, Memory: 125632 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,14 +1,14 @@
 import sys
+input = sys.stdin.readline
 
-N = int(sys.stdin.readline())
-X = list(map(int, sys.stdin.readline().split()))
+N = int(input())
+X = set(map(int, sys.stdin.readline().split()))
 M = int(sys.stdin.readline())
 Y = list(map(int, sys.stdin.readline().split()))
 
-XnY = set(X) & set(Y)
 count = [0] * M
 for i in range(M):
-    if Y[i] in XnY:
+    if Y[i] in X:
         count[i] = 1
 
 print(*count)
