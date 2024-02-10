7232fa797dc2401cb0345367d6f2d4b62c82b4e2
mkkim68 <116176465+mkkim68@users.noreply.github.com>
2024-02-10 15:39:00 +0900

    [Bronze II] Title: Hashing, Time: 116 ms, Memory: 108080 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+L = int(input())
+string = input()[:-1]
+
+index = [i for i in range(1, 27)]
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alpha_dict = {alphabet[i]:index[i] for i in range(26)}
+
+ans = 0
+for s in range(len(string)):
+    ans += alpha_dict[string[s]] * 31 ** s
+
+print(ans)
