95e79729f5036bb1b1508b652d976b3afccf0f94
mkkim68 <116176465+mkkim68@users.noreply.github.com>
2024-03-07 12:55:57 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int X, N, a, b;
+	int total = 0;
+	cin >> X;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> a >> b;
+		total += a * b;
+	}
+	if (total == X) {
+		cout << "Yes";
+	}
+	else {
+		cout << "No";
+	}
+	return 0;
+}
