97757a66856e4b430b8e871522deafc00f3dd164
mkkim68 <116176465+mkkim68@users.noreply.github.com>
2024-06-20 23:23:12 +0900

    [Gold II] Title: 트리의 지름, Time: 1000 ms, Memory: 74428 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+V = int(input())
+edges = [[] for _ in range(V+1)]
+
+for _ in range(V):
+    node, *arr, last = map(int, input().split())
+    for i in range(0, len(arr), 2):
+        next, weight = arr[i], arr[i+1]
+        edges[node].append([next, weight])
+
+visited = [-1] * (V+1)
+visited[1] = 0
+
+def dfs(node, dist):
+    for v, d in edges[node]:
+        cal_dist = dist + d
+        if visited[v] == -1:
+            visited[v] = cal_dist
+            dfs(v, cal_dist)
+    return
+
+dfs(1, 0)
+tmp = [0, 0]
+
+for i in range(1, V+1):
+    if visited[i] > tmp[1]:
+        tmp[1] = visited[i]
+        tmp[0] = i
+
+visited = [-1] * (V+1)
+visited[tmp[0]] = 0
+dfs(tmp[0], 0)
+
+print(max(visited))
