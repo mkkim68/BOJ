de432f4afd74c8ea02ab8473084a6eddc43a9ddd
김민경 <116176465+mkkim68@users.noreply.github.com>
2025-03-19 14:48:45 +0900

    [Gold IV] Title: 어드벤처 게임, Time: 132 ms, Memory: 112572 KB -BaekjoonHub

--- 백준/Gold/2310. 어드벤처 게임/어드벤처 게임.py
+++ 백준/Gold/2310. 어드벤처 게임/어드벤처 게임.py
@@ -1,28 +1,6 @@
 import sys
-sys.setrecursionlimit(10**6)
+from heapq import heappop, heappush
 input = sys.stdin.readline
-
-def dfs(cur, money):
-    global visited
-
-    if rooms[cur] > 0:
-        money = max(money, rooms[cur])
-    else:
-        money += rooms[cur]
-        if money < 0:
-            return False
-
-    if cur == N:
-        return True
-
-    visited[cur] = 1
-    for next in edges[cur]:
-        if visited[next] == 0:
-            if dfs(next, money):
-                return True
-    visited[cur] = 0
-    return False
-
 
 
 while True:
@@ -35,15 +13,30 @@ while True:
     edges = [[]]  # 방 연결 정보
     for i in range(1, N+1):
         info, cost, *next_rooms, end = input().strip().split()
-        if info == 'T':
+        if info == 'L':
             rooms.append(-int(cost))
         else:
             rooms.append(int(cost))
 
         edges.append(list(map(int, next_rooms)))
 
-    visited = [0] * (N+1)
-    if dfs(1, 0):
+    pq = []
+    visited = [0] * (N + 1)
+    heappush(pq, (0, 1))
+    while pq:
+        money, now = heappop(pq)
+        visited[now] = 1
+        for next in edges[now]:
+            if visited[next]:
+                continue
+            if rooms[next] > 0:
+                if money + rooms[next] > 0:
+                    continue
+                heappush(pq, (money + rooms[next], next))
+            else:
+                heappush(pq, (min(money, rooms[next]), next))
+
+    if visited[N]:
         print("Yes")
     else:
-        print("No")
+        print('No')
