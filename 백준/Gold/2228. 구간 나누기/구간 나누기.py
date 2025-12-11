7667dd7af346c4ae271f71a540fbb12716b0d935
김민경 <116176465+mkkim68@users.noreply.github.com>
2025-12-11 19:11:19 +0900

    [Gold III] Title: 구간 나누기, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Gold/2228. 구간 나누기/구간 나누기.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+notcon = [[0] + [-1e9] * M for _ in range(N+1)]
+con = [[0] + [-1e9] * M for _ in range(N+1)]
+
+for i in range(1, N+1):
+    num = int(input())
+    for j in range(1, min(M, (i+1)//2)+1):
+        notcon[i][j] = max(con[i-1][j], notcon[i-1][j])
+        con[i][j] = max(con[i-1][j], notcon[i-1][j-1]) + num
+
+print(max(con[-1][-1], notcon[-1][-1]))
