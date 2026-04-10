052c50b9be6d9bd176a4cf4376777ba077564093
김민경 <116176465+mkkim68@users.noreply.github.com>
2026-04-10 13:39:25 +0900

    [Gold III] Title: 비슷한 단어, Time: 6344 ms, Memory: 114424 KB -BaekjoonHub

---
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+words = [input().strip() for _ in range(N)]
+nums = [i for i in range(N)]
+
+def combi(idx, arr):
+    global ans1, ans2, cnt
+
+    if len(arr) == 2:
+        i1, i2 = arr
+        word1, word2 = words[i1], words[i2]
+        M = min(len(word1), len(word2))
+        for i in range(M):
+            if word1[i] != word2[i]:
+                if i > cnt:
+                    cnt = i
+                    ans1, ans2 = i1, i2
+                break
+        else:
+            if cnt < M:
+                cnt = M
+                ans1, ans2 = i1, i2
+        return
+
+    for i in range(idx, N):
+        if not visited[i]:
+            visited[i] = 1
+            combi(i+1, arr+[i])
+            visited[i] = 0
+
+
+
+ans1, ans2 = -1, -1
+cnt = -1
+visited = [0] * N
+
+combi(0, [])
+print(words[ans1])
+print(words[ans2])
