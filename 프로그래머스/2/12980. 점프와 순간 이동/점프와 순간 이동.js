df75bf8c165f643ef946c3011271854408e6e89a
김민경 <116176465+mkkim68@users.noreply.github.com>
2026-05-05 18:34:05 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,15 @@
+function solution(n)
+{
+    let cost = 0;
+    
+    while (n > 0) {
+        if (n % 2 === 0) {
+            n = n / 2;
+        } else {
+            n -= 1;
+            cost++;
+        }
+    }
+    
+    return cost;
+}
